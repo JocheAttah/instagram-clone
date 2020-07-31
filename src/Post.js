b41f227d8450e397d623017432ba3abf7622fa53
Attah David <jocheattahdavid@gmail.com>
2020-07-31 22:11:45 +0100

    header layout section

---
+++ src/Post.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div className='Post'>
+            {/* avartar + username */}
+
+            {/* image */}
+
+
+            {/* username + caption */}
+        </div>
+    )
+}
+
+export default Post
